4f2a589d7dab0ecfcb65f893b79fae4f3fd7c6a8
noobdawn <694109410@qq.com>
2018-09-01 18:47:57 +0800

    First Sumbit

---
+++ Assets/Scripts/GameMain.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cell
+{
+    public int x;
+    public int y;
+    public bool isAliveNow;
+    public bool isAliveNext;
+    public Image imageCom;
+    public List<Cell> neighbors;
+    public Cell(int x, int y, bool isAlive)
+    {
+        this.x = x;
+        this.y = y;
+        isAliveNow = isAlive;
+        isAliveNext = false;
+        imageCom = null;
+        neighbors = new List<Cell>();
+    }
+
+    public void ShowNow()
+    {
+        if (imageCom == null)
+            return;
+        imageCom.color = isAliveNow ? Color.black : Color.white;
+    }
+
+    public void UpdateNext()
+    {
+        isAliveNow = isAliveNext;
+        isAliveNext = false;
+    }
+
+    public void SetNext(bool b)
+    {
+        isAliveNext = b;
+    }
+
+    public int GetAliveNeighborNum()
+    {
+        int res = 0;
+        foreach(Cell neighbor in neighbors)
+        {
+            if (neighbor != null && neighbor.isAliveNow)
+                res++;
+        }
+        return res;
+    }
+}
+
+public class GameMain : MonoBehaviour {
+
+    [SerializeField]
+    private Texture2D inputTex;
+    private int GameWidth;
+    private int GameHeight;
+
+
+    private const int SIZE_OF_CELL = 2;
+
+    private List<Cell> cells;
+
+	void Start () {
+        InitByInputTex();
+        InitMatrix();
+	}
+
+    void Update()
+    {
+        if (inputTex == null) return;
+        foreach(Cell cell in cells)
+        {
+            int num = cell.GetAliveNeighborNum();
+            if (cell.isAliveNow)
+            {
+                if (num < 2)
+                    cell.SetNext(false);
+                else if (num < 4)
+                    cell.SetNext(cell.isAliveNow);
+                else
+                    cell.SetNext(false);
+            }
+            else if (num == 3)
+                cell.SetNext(true);
+        }
+        foreach(Cell cell in cells)
+        {
+            cell.ShowNow();
+            cell.UpdateNext();
+        }
+    }
+
+    private void InitByInputTex()
+    {
+        if (inputTex == null)
+            return;
+        GameWidth = inputTex.width;
+        GameHeight = inputTex.height;
+        cells = new List<Cell>();
+        for(int y = 0; y < GameHeight; y++)
+            for (int x = 0; x < GameWidth; x++)
+            {
+                Color c = inputTex.GetPixel(x, y);
+                cells.Add(new Cell(x, y, c.a > 0));
+            }
+    }
+
+    private void InitMatrix()
+    {
+        Transform templet = transform.Find("CellTemplet");
+        if (templet == null)
+            return;
+        for (int y = 0; y < GameHeight; y++)
+            for (int x = 0; x < GameWidth; x++)
+            {
+                GameObject cell = Object.Instantiate<GameObject>(templet.gameObject);
+                cell.transform.parent = transform;
+                cell.transform.localPosition = new Vector3((x - (GameWidth >> 1)) * SIZE_OF_CELL, (y - (GameHeight >> 1)) * SIZE_OF_CELL, 0);
+                cell.SetActive(true);
+                cells[x + y * GameWidth].imageCom = cell.GetComponent<Image>();
+            }
+        foreach (Cell cell in cells)
+        {
+            cell.neighbors.Add(GetCell(cell.x - 1, cell.y - 1));
+            cell.neighbors.Add(GetCell(cell.x - 1, cell.y));
+            cell.neighbors.Add(GetCell(cell.x - 1, cell.y + 1));
+            cell.neighbors.Add(GetCell(cell.x, cell.y - 1));
+            cell.neighbors.Add(GetCell(cell.x, cell.y + 1));
+            cell.neighbors.Add(GetCell(cell.x + 1, cell.y - 1));
+            cell.neighbors.Add(GetCell(cell.x + 1, cell.y));
+            cell.neighbors.Add(GetCell(cell.x + 1, cell.y + 1));
+        }
+    }
+
+    private Cell GetCell(int x, int y)
+    {
+        if (x < 0 || x >= GameWidth || y < 0 || y >= GameHeight)
+            return null;
+        return cells[x + y * GameWidth];
+    }
+
+}
---
+++ Assets/Scripts/SendTex.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct GPUCell
+{
+    public Vector4 xynn;
+    public GPUCell(float x, float y, float z, float w)
+    {
+        xynn = new Vector4(x, y, z, w);
+    }
+}
+
+public class SendTex : MonoBehaviour {
+    public ComputeShader GoLComputeShader;
+    public Texture InputTex;
+    public Color AliveColor;
+    public Color DeadColor;
+    public Renderer Shower;
+    [SerializeField]
+    private RenderTexture GameTex;
+    private ComputeBuffer CellBuffer;
+    private bool needRendering = false;
+    int kernelIdx;
+    int updateIdx;
+
+    #region Mono
+    void OnGUI()
+    {
+        if (GUILayout.Button("开始生命游戏"))
+        {
+            Init();            
+            needRendering = true;
+        }
+    }
+	
+	void Update () {
+        if (!needRendering)
+            return;
+       
+        //运算
+        GoLComputeShader.Dispatch(kernelIdx, Mathf.CeilToInt(InputTex.width / 32f), Mathf.CeilToInt(InputTex.height / 32f), 1);
+        GoLComputeShader.Dispatch(updateIdx, Mathf.CeilToInt(InputTex.width / 32f), Mathf.CeilToInt(InputTex.height / 32f), 1);
+       
+	}
+    #endregion
+
+    #region Logic
+    private void Init()
+    {
+        //找到核心地址
+        kernelIdx = GoLComputeShader.FindKernel("GameOfLifeUpdate");
+        updateIdx = GoLComputeShader.FindKernel("UpdateNext");
+        InitTexture();
+        InitColor();
+        InitBuffer();
+        Shower.material.mainTexture = GameTex;
+    }
+
+    private void InitColor()
+    {
+        GoLComputeShader.SetVector("AliveColor", AliveColor);
+        GoLComputeShader.SetVector("DeadColor", DeadColor);
+    }
+
+    private void InitTexture()
+    {
+        //传入展示和逻辑用纹理
+        GameTex = new RenderTexture(InputTex.width, InputTex.height, 24);
+        GameTex.enableRandomWrite = true;
+        GameTex.filterMode = FilterMode.Point;
+        GameTex.Create();
+        GoLComputeShader.SetTexture(kernelIdx, "GameTex", GameTex);
+        //传入边界
+        GoLComputeShader.SetVector("BorderVec", new Vector4(0, InputTex.width, 0, InputTex.height));
+    }
+
+    private void InitBuffer()
+    {
+        int count = InputTex.width * InputTex.height;
+        CellBuffer = new ComputeBuffer(count, 16);
+        GPUCell[] values = new GPUCell[count];
+        for (int y = 0 ; y < InputTex.height; y++)
+            for (int x = 0; x < InputTex.width; x++)
+            {
+                Color c = ((Texture2D)InputTex).GetPixel(x, y);
+                GPUCell cell = new GPUCell(x, y,
+                    c.a != 0 ? 1 : 0, c.a != 0 ? 1 : 0);
+                values[x + y * InputTex.width] = cell;
+            }
+        //装填数据
+        CellBuffer.SetData(values);
+        GoLComputeShader.SetBuffer(kernelIdx, "Cells", CellBuffer);
+        GoLComputeShader.SetBuffer(updateIdx, "Cells", CellBuffer);
+    }
+    #endregion
+}
